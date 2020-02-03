072ddab4368503288fb1ec7731b33ea0c6f0bcec
Biswajit Biswas <42512360+jfBiswajit@users.noreply.github.com>
2020-02-03 20:29:59 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
 <?php
-$getJokesJSON = file_get_contents('http://api.icndb.com/jokes/random/3');
-$getJokesObj = json_decode($getJokesJSON);
-
-print_r($getJokesObj->value[1]->joke);
+$json = file_get_contents('http://api.icndb.com/jokes/random/3');
+$obj = json_decode($getJokesJSON);
